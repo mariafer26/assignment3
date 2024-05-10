1b1605d47674383d6d6891cb6994ac7eccc9b606
Fernanda26 <mfalvarezm@eafit.edu.co>
2024-05-10 17:04:02 -0500

    cambios del proyecto

---
+++ main.java
@@ -0,0 +1,9 @@
+public class main{
+    public static void printName(){
+        System.out.println("Assignment 3 by maria fernanda alvarez marin");
+    }
+
+public static void main(String[] args) {
+    printName();
+}
+}
